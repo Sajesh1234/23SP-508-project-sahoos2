682734142c97ac55f1f009d0cdb3ab0f499d3259
Sajesh1234 <69909148+Sajesh1234@users.noreply.github.com>
2023-04-22 15:10:24 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 <title>TCG Database</title>
 <style>
     body{
-        width: 100%;
-        height:100%;
+        width: 99%;
+        height:97%;
         font-family: 'Open Sans', sans-serif;
         background: #092756;
         background: -moz-radial-gradient(0% 100%, ellipse cover, rgba(104,128,138,.4) 10%,rgba(138,114,76,0) 40%),-moz-linear-gradient(top,  rgba(57,173,219,.25) 0%, rgba(42,60,87,.4) 100%), -moz-linear-gradient(-45deg,  #670d10 0%, #092756 100%);
@@ -35,9 +35,12 @@
 
 <body>
   
-  <a href = "/~23SP_sahoos2/login.php" class="login-button"><button>Login/ Sign Up</button></a>
-  <a herf = "/~23SP_sahoos2/teams.php" class="teams-button"><button>Teams</button></a>
-  <a herf = "/~23SP_sahoos2/cards.php" class="cards-button"><button>Cards</button></a>
+  <a href = "login" class="login-button"><button>Login/ Sign Up</button></a>
+  <ul>
+  <li><a href="teams">Teams</a></li>
+  
+  </ul>
+  <a href = "/~23SP_sahoos2/cards.php" class="cards-button"><button>Cards</button></a>
   
 </body>
 </html>
