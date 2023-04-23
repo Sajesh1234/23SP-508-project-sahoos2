beae258c6115b0bf425c30dbe065eaa1c4afad0c
Minescratcher452 <minescratcher452@gmail.com>
2023-04-22 23:34:49 -0400

    Update signup.php

--- signup.php
+++ signup.php
@@ -6,6 +6,10 @@
 
 <?php require_once('connection.php'); 
 
+if(!empty($_POST['action']) && $_POST['action'] == 'signup.php') {
+	echo $_POST['firstname'] + " " + $_POST['lastname'];
+}
+
 
 ?>
 <style>
@@ -100,7 +104,7 @@ input:focus { box-shadow: inset 0 -5px 45px rgba(100,100,100,0.4), 0 1px 1px rgb
 
 	<div class="Signup">
 	<h1>SIGN UP</h1>
-		<form method="post">
+		<form action="signup.php" method="post">
 			<label for="email">Email:</label>
 			<input type="text" name="email" placeholder="Email" required="required" />
 			<label for="firstname">First Name:</label>
