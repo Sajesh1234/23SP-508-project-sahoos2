e4989d98c7312e2cfd9dd3c240162233112e5cc6
Minescratcher452 <minescratcher452@gmail.com>
2023-04-22 22:36:18 -0400

    post testing

---
+++ login-landing.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["First Name"]; echo $_POST["Last Name"]; ?><br>
+Your email address is: <?php echo $_POST["Email"]; ?>
+
+</body>
+</html> 
--- signup.php
+++ signup.php
@@ -97,7 +97,7 @@ input:focus { box-shadow: inset 0 -5px 45px rgba(100,100,100,0.4), 0 1px 1px rgb
 
 	<div class="Signup">
 	<h1>SIGN UP</h1>
-		<form method="post">
+		<form action="login-landing.php" method="post">
 			<label for="Email">Email:</label>
 			<input type="text" name="Email" placeholder="Email" required="required" />
 			<label for="First Name">First Name:</label>
