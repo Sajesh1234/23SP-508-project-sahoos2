d9bf78943f9d3afc4dd73f56939cec49598cddd1
Minescratcher452 <minescratcher452@gmail.com>
2023-04-23 21:50:27 -0400

    ref only

--- ref-only.php
+++ ref-only.php
@@ -8,10 +8,10 @@ require_once('login-locked.php');
 
 if (!empty($_SESSION['user_type']))
 {
-	// If the user_type session is not "Player", then the user can't see this page
-	if($_SESSION['user_type'] != "player") {
+	// If the user_type session is not "ref", then the user can't see this page
+	if($_SESSION['user_type'] != "ref") {
 		header("Location: index.php");
-		echo "You must be a player to see that page.";
+		echo "You must be a ref to see that page.";
 	}
 	
 
