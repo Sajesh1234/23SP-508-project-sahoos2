6c912561f763dd4ad7a30fac38e67b1f26d657cd
Minescratcher452 <minescratcher452@gmail.com>
2023-04-23 20:56:13 -0400

    Update connection.php

--- connection.php
+++ connection.php
@@ -5,12 +5,12 @@ ini_set('display_errors', 1);
 
 $servername = "cmsc508.com";
 $username = "23SP_sahoos2";     // Replace yourVCUeid
-$password = "23SP_sahoos2";     // Replace yourVCUeid
+$db_password = "23SP_sahoos2";     // Replace yourVCUeid
 $database = "23SP_sahoos2_pr";  // Replace yourVCUeid
 
 // Connect to the MySQL server
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $db_password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);    
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
