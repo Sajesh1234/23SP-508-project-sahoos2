c021aabaf3f2e00c3ddf53179999bb0704466b95
Minescratcher452 <minescratcher452@gmail.com>
2023-04-23 21:48:28 -0400

    Update signup.php

--- signup.php
+++ signup.php
@@ -143,7 +143,7 @@ input:focus { box-shadow: inset 0 -5px 45px rgba(100,100,100,0.4), 0 1px 1px rgb
 			<select id="acc" name = "acc">
 				<option value = "">N/A</option>
 				<option value = "player">Player</option>
-				<option value = "referee">Referee</option>
+				<option value = "ref">Referee</option>
 			</select>
 			<button type="submit" class="btn btn-primary btn-block btn-large">Create Account</button>
 		</form>
