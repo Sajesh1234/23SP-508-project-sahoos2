1be1a52d98efed61fa65b7ac5a7ae9e1b1ede0c7
Sajesh1234 <69909148+Sajesh1234@users.noreply.github.com>
2023-04-26 22:10:57 -0400

    Add files via upload
    changed index added picture and overall made it look a lot nicer

--- index.php
+++ index.php
@@ -1,47 +1,168 @@
+<?php require_once('header.php'); ?>
+
 <html>
 <head>
-<title>TCG Database</title>
-<style>
-    body{
-        width: 99%;
-        height:97%;
-        font-family: 'Open Sans', sans-serif;
-        background: #092756;
-        background: -moz-radial-gradient(0% 100%, ellipse cover, rgba(104,128,138,.4) 10%,rgba(138,114,76,0) 40%),-moz-linear-gradient(top,  rgba(57,173,219,.25) 0%, rgba(42,60,87,.4) 100%), -moz-linear-gradient(-45deg,  #670d10 0%, #092756 100%);
-        background: -webkit-radial-gradient(0% 100%, ellipse cover, rgba(104,128,138,.4) 10%,rgba(138,114,76,0) 40%), -webkit-linear-gradient(top,  rgba(57,173,219,.25) 0%,rgba(42,60,87,.4) 100%), -webkit-linear-gradient(-45deg,  #670d10 0%,#092756 100%);
-        background: -o-radial-gradient(0% 100%, ellipse cover, rgba(104,128,138,.4) 10%,rgba(138,114,76,0) 40%), -o-linear-gradient(top,  rgba(57,173,219,.25) 0%,rgba(42,60,87,.4) 100%), -o-linear-gradient(-45deg,  #670d10 0%,#092756 100%);
-        background: -ms-radial-gradient(0% 100%, ellipse cover, rgba(104,128,138,.4) 10%,rgba(138,114,76,0) 40%), -ms-linear-gradient(top,  rgba(57,173,219,.25) 0%,rgba(42,60,87,.4) 100%), -ms-linear-gradient(-45deg,  #670d10 0%,#092756 100%);
-        background: -webkit-radial-gradient(0% 100%, ellipse cover, rgba(104,128,138,.4) 10%,rgba(138,114,76,0) 40%), linear-gradient(to bottom,  rgba(57,173,219,.25) 0%,rgba(42,60,87,.4) 100%), linear-gradient(135deg,  #670d10 0%,#092756 100%);
-        filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#3E1D6D', endColorstr='#092756',GradientType=1 );
-        }
-    .login-button {
-            float: right;
-            margin-right: 0px;
-        }
-    .teams-button {
-            position: fixed;
-            top: 50%;
-            left: 10px;
-            transform: translateY(-50%);
-    }
-    .cards-button {
-            position:fixed;
-            top:60%;
-            left: 10px;
-            transform:translateY(-50%);
-    }
-
-</style>
-
+	<title>TCG DATABASE</title>
+	<style>
+	*{
+	   margin: 0;
+	   padding: 0;
+     	}
+     	header{
+     	  height: 100px;
+     	  background: rgba(0,0,0,0.7);
+     	  width: 100%;
+     	  position:fixed;
+     	  z-index:12;
+     	}
+	   .logo{
+     	  font-family: 'Castoro Titling';
+     	  font-size: 25px;
+     	  padding: 50px;
+     	  margin-left:50px;
+     	}
+     	.logo a{
+     	  color: #fff;
+     	  text-decoration: none;
+     	}
+     	.nav_bar{
+     	  font-family: 'Castoro Titling';
+     	  font-size: 20px;
+     	  float:right;
+     	  list-style: none;
+     	}
+     	.nav_bar li{
+     	  display: inline-block;
+     	}
+	    .nav_bar li a{
+     	  padding: 15px;
+     	}
+	    .nav_bar li a:hover{
+     	  bacjground: #B4AFAF;
+     	  color:#fff;
+     	  border: 1px solid #000;
+     	  border-radius: 5px;
+     	  border-width: auto;
+     	}
+     	.imagesfit{
+     	height:975px;
+     	width: 1920px;
+     	object-fit: fill;
+     	filter:brightness(80%);
+     	}
+     	.box{
+     	background:rgba(0, 0, 0, 0.5);
+     	width:100%;
+     	padding: 25px;
+     	color:white;
+     	
+     	}
+     	
+	</style>
+	
+</head>
 <body>
-  
-  <a href = "login" class="login-button"><button>Login/ Sign Up</button></a>
-  <a href = "logout" class="login-button"><button>Logout</button></a>   
-  <ul>
-  <li><a href="teams">Teams</a></li>
-  
-  </ul>
-  <a href = "/~23SP_sahoos2/cards.php" class="cards-button"><button>Cards</button></a>
-  
+<header>
+	<div class ="logo">
+	<a href ="index.php" >TCG DataBase</a>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
+		<div class ="nav_bar">
+			<ul>
+				<li><a href = "teams">Teams</a></li>
+				<li><a href = "cards"> Cards </a></li>
+				<li><a href = "login" >Login | Sign Up</a></li>
+				
+			</ul>
+		</div>
+	</div>
+</header>
+
+<div id="carouselExampleDark" class="carousel carousel-dark slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="3" aria-label="Slide 4"></button>
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="4" aria-label="Slide 5"></button>
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="5" aria-label="Slide 6"></button>
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="6" aria-label="Slide 7"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="10000">
+      <img class = "imagesfit" src="images/dragon.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+      <div class ="box">
+        <h1>Blue Eyes White Dragon</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+      <img class = "imagesfit" src="images/armor.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+      <div class ="box">
+        <h1>Enchanted Armor</h1>
+        <p>Some representative placeholder content for the second slide.</p>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img class = "imagesfit" src="images/ghast.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+      <div class ="box">
+        <h1>Ghast</h1>
+        <p>Some representative placeholder content for the third slide.</p>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img class = "imagesfit" src="images/blademaster.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+      <div class ="box">
+        <h1>Blademaster</h1>
+        <p>Some representative placeholder content for the third slide.</p>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img class = "imagesfit" src="images/giant.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+      <div class ="box">
+        <h1>Giant</h1>
+        <p>Some representative placeholder content for the third slide.</p>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img class = "imagesfit" src="images/witchC.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+      <div class ="box"> 
+        <h1>Mage Doctorate</h1>
+        <p>Some representative placeholder content for the third slide.</p>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img class = "imagesfit" src="images/Wyrmling2.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+      	<div class ="box">
+        <h1>Third slide label</h1>
+        <p>Some representative placeholder content for the third slide.</p>
+        </div>
+      </div>
+    </div>
+    
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
 </body>
 </html>
