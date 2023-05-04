861a50b80bba88df393ac4c12e583fe05007e18b
Minescratcher452 <minescratcher452@gmail.com>
2023-05-04 14:46:24 -0400

    cards view

--- cards.php
+++ cards.php
@@ -1,4 +1,30 @@
-<?php require('player-only.php');
-echo "This is a barebones authorization testing page.";
-?>
+<html>
+<head>
+<title>TCG Cards</title>
+<?php require_once('header.php'); ?>
 
+<script src="js/cards.js"></script>
+</head>
+
+<?php require_once('connection.php'); ?>
+
+<body>
+
+<div class="container-fluid mt-3 mb-3">
+	<h4>Teams</h4>
+	<div>
+		<table id="table-Card" class="table table-bordered table-striped">
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Rarity</th>
+					<th>Text</th>
+					<th>Expansion</th>
+				</tr>
+			</thead>
+		</table>
+	</div>
+</div>
+
+</body>
+</html>
---
+++ js/cards.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	
+	$('#table-Card').DataTable({
+		"dom": 'Blfrtip',
+		"ordering": false,
+		"searching": false,
+		"paging": false,
+		"responsive": true,
+		"ajax":{
+			url:"card-action.php",
+			type:"POST",
+			data:{
+					action:'listCards'
+				 },
+			dataType:"json"
+		}
+	});
+	
+});
