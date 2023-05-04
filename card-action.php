2fc0652edd3bac4f87f594b094d45d111d7e0c97
Minescratcher452 <minescratcher452@gmail.com>
2023-05-04 15:09:44 -0400

    Update card-action.php

--- card-action.php
+++ card-action.php
@@ -5,22 +5,7 @@ global $conn;
 
 function listCards()
 {
-    global $conn;
-    
-    $sqlQuery = "SELECT *
-                 FROM
-                 Card_View";
-    
-    $stmt = $conn->prepare($sqlQuery);
-    $stmt->execute();
-    
-    $numberRows = $stmt->rowCount();
-    
-    $data = array();
-    while ($row = mysql_fetch_assoc($results)) {
-       $data[] = $row;
-    }
-    echo json_encode($data);
+    echo "[{id:'e'}]";
 }
 
 
