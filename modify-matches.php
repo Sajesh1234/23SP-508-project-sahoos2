74d30ce3b7bfc92416abca88f69cab4b9896beeb
Sajesh1234 <69909148+Sajesh1234@users.noreply.github.com>
2023-05-06 03:00:19 -0400

    Add files via upload
    changed all the admin modification frontend

--- modify-matches.php
+++ modify-matches.php
@@ -21,12 +21,48 @@
 </head>
 
 <?php require_once('connection.php'); global $conn; ?>
+<style>
+body { 
+  width: 100%;
+  height:100%;
+  font-family: 'Open Sans', sans-serif;
+  background: #092756;
+  background: -moz-radial-gradient(0% 100%, ellipse cover, rgba(104,128,138,.4) 10%,rgba(138,114,76,0) 40%),-moz-linear-gradient(top,  rgba(57,173,219,.25) 0%, rgba(42,60,87,.4) 100%), -moz-linear-gradient(-45deg,  #670d10 0%, #092756 100%);
+  background: -webkit-radial-gradient(0% 100%, ellipse cover, rgba(104,128,138,.4) 10%,rgba(138,114,76,0) 40%), -webkit-linear-gradient(top,  rgba(57,173,219,.25) 0%,rgba(42,60,87,.4) 100%), -webkit-linear-gradient(-45deg,  #670d10 0%,#092756 100%);
+  background: -o-radial-gradient(0% 100%, ellipse cover, rgba(104,128,138,.4) 10%,rgba(138,114,76,0) 40%), -o-linear-gradient(top,  rgba(57,173,219,.25) 0%,rgba(42,60,87,.4) 100%), -o-linear-gradient(-45deg,  #670d10 0%,#092756 100%);
+  background: -ms-radial-gradient(0% 100%, ellipse cover, rgba(104,128,138,.4) 10%,rgba(138,114,76,0) 40%), -ms-linear-gradient(top,  rgba(57,173,219,.25) 0%,rgba(42,60,87,.4) 100%), -ms-linear-gradient(-45deg,  #670d10 0%,#092756 100%);
+  background: -webkit-radial-gradient(0% 100%, ellipse cover, rgba(104,128,138,.4) 10%,rgba(138,114,76,0) 40%), linear-gradient(to bottom,  rgba(57,173,219,.25) 0%,rgba(42,60,87,.4) 100%), linear-gradient(135deg,  #670d10 0%,#092756 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#3E1D6D', endColorstr='#092756',GradientType=1 );
+}
+tr { 
+background-color: #a6a6a6; }
+
+.box{
+     	padding: 10px;
+        background:#AAA3B8;
+     	
+     	}
+ tbody td:nth-child(odd) {
+    background-color: #CFBBF7;
+}
+
+
+tbody td:nth-child(even) {
+  background-color: #CFBBF7;
+}
+.matches{
+  	width: 100%;
+    display: flex;
+    flex-direction: column;
+     	}
+</style>
 
 <body>
 
-<div class="container-fluid mt-3 mb-3">
-	<h4>Match Information</h4>
+<div class="matches">
+	<h4 style = "text-align:center; color: white;">Match Information</h4>
 	
+	<div class ="box">
 	<div class="pb-3">
 		<button type="button" id="addMatch" class="btn btn-primary btn-sm">Add Match Info</button>
 	</div> 
@@ -45,6 +81,7 @@
 				</tr>
 			</thead>
 		</table>
+	</div>
 	</div>
 </div>
 
--- modify-players.php
+++ modify-players.php
@@ -19,14 +19,49 @@
 <!-- CSS for datatables buttons -->
 <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.3.6/css/buttons.dataTables.min.css"/>
 </head>
+<style>
+body { 
+  width: 100%;
+  height:100%;
+  font-family: 'Open Sans', sans-serif;
+  background: #092756;
+  background: -moz-radial-gradient(0% 100%, ellipse cover, rgba(104,128,138,.4) 10%,rgba(138,114,76,0) 40%),-moz-linear-gradient(top,  rgba(57,173,219,.25) 0%, rgba(42,60,87,.4) 100%), -moz-linear-gradient(-45deg,  #670d10 0%, #092756 100%);
+  background: -webkit-radial-gradient(0% 100%, ellipse cover, rgba(104,128,138,.4) 10%,rgba(138,114,76,0) 40%), -webkit-linear-gradient(top,  rgba(57,173,219,.25) 0%,rgba(42,60,87,.4) 100%), -webkit-linear-gradient(-45deg,  #670d10 0%,#092756 100%);
+  background: -o-radial-gradient(0% 100%, ellipse cover, rgba(104,128,138,.4) 10%,rgba(138,114,76,0) 40%), -o-linear-gradient(top,  rgba(57,173,219,.25) 0%,rgba(42,60,87,.4) 100%), -o-linear-gradient(-45deg,  #670d10 0%,#092756 100%);
+  background: -ms-radial-gradient(0% 100%, ellipse cover, rgba(104,128,138,.4) 10%,rgba(138,114,76,0) 40%), -ms-linear-gradient(top,  rgba(57,173,219,.25) 0%,rgba(42,60,87,.4) 100%), -ms-linear-gradient(-45deg,  #670d10 0%,#092756 100%);
+  background: -webkit-radial-gradient(0% 100%, ellipse cover, rgba(104,128,138,.4) 10%,rgba(138,114,76,0) 40%), linear-gradient(to bottom,  rgba(57,173,219,.25) 0%,rgba(42,60,87,.4) 100%), linear-gradient(135deg,  #670d10 0%,#092756 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#3E1D6D', endColorstr='#092756',GradientType=1 );
+}
+tr { 
+background-color: #a6a6a6; }
+
+.box{
+     	padding: 10px;
+        background:#AAA3B8;
+     	
+     	}
+ tbody td:nth-child(odd) {
+    background-color: #CFBBF7;
+}
+
+
+tbody td:nth-child(even) {
+  background-color: #CFBBF7;
+}
+.player{
+  	width: 100%;
+    display: flex;
+    flex-direction: column;
+     	}
+</style>
 
 <?php require_once('connection.php'); global $conn; ?>
 
 <body>
 
-<div class="container-fluid mt-3 mb-3">
-	<h4>Player Information</h4>
-	
+<div class="player">
+	<h4 style = "text-align:center; color: white;">Player Information</h4>
+	<div class ="box">
 	<div class="pb-3">
 		<button type="button" id="addPlayer" class="btn btn-primary btn-sm">Add Player Info</button>
 	</div> 
@@ -46,6 +81,7 @@
 				</tr>
 			</thead>
 		</table>
+	</div>
 	</div>
 </div>
 
