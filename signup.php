881ff522820f746b21426dfc9c6d6d363bfafebb
Minescratcher452 <minescratcher452@gmail.com>
2023-05-06 20:46:55 -0400

    Update signup.php

--- signup.php
+++ signup.php
@@ -25,7 +25,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 
 		// Insert into player or ref tables as appropriate
 		if ($_POST['acc'] = "Player") {
-			$stmt = $conn->prepare("INSERT INTO Player (Person_ID, Wins, Draws, Losses, Play_count) VALUES (:email, 0, 0, 0, 0)");
+			$stmt = $conn->prepare("INSERT INTO Player (Person_ID, Wins, Draws, Losses, Play_count) VALUES (:email, 0, 0, 0, 1)");
 			$stmt->bindValue(':email', $_POST['email']);
 			$stmt->execute();
 		} elseif ($_POST['acc'] = "Ref") {
